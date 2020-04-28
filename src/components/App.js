73e52c8134e488b74b4decad0ae39b55f2606726
william <mbouna.william@athena.cm>
2020-04-28 15:50:53 +0100

    client :: init sub project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>App</div>
+        );
+    };
+};
+
+
+export default App;
